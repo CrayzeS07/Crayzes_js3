ac2bd301c3fb00208680748941adedd94f1190b4
Bexruz <141207743+CrayzeS07@users.noreply.github.com>
2023-11-09 20:16:02 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,79 @@
-// // sozlarni teskari tartibda chiqrish
-{let a = prompt("so'z kiring")
-let b = a.split('').reverse().join('');
-console.log(b);}
-//sonlarni ozgartirish
-    let son = prompt("son kiring");
-    let oxirgiSon = parseInt(son.toString().split('').reverse().join(''));
-    console.log(oxirgiSon); 
-//sonlarni kattasini aniqlash
-let a = parseInt(Math.random() * 2)
-let b = parseInt(Math.random() * 2)
-let c = parseInt(Math.random() * 2)
+const elts = {
+    text1: document.getElementById("text1"),
+    text2: document.getElementById("text2")
+};
 
+const texts = [
+    ":)",
+    "@CrayzeS_777"
+];
 
-if(a > b){
-    if (a > c){
-        console.log(`${a} son katta`);
-    }else{
-        console.log(`${c} son katta`);
+const morphTime = 1;
+const cooldownTime = 0.25;
+
+let textIndex = texts.length - 1;
+let time = new Date();
+let morph = 0;
+let cooldown = cooldownTime;
+
+elts.text1.textContent = texts[textIndex % texts.length];
+elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+
+function doMorph() {
+    morph -= cooldown;
+    cooldown = 0;
+
+    let fraction = morph / morphTime;
+
+    if (fraction > 1) {
+        cooldown = cooldownTime;
+        fraction = 1;
+    }
+
+    setMorph(fraction);
+}
+
+function setMorph(fraction) {
+    elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    fraction = 1 - fraction;
+    elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
+    elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
+
+    elts.text1.textContent = texts[textIndex % texts.length];
+    elts.text2.textContent = texts[(textIndex + 1) % texts.length];
+}
+
+function doCooldown() {
+    morph = 0;
+
+    elts.text2.style.filter = "";
+    elts.text2.style.opacity = "100%";
+
+    elts.text1.style.filter = "";
+    elts.text1.style.opacity = "0%";
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    let newTime = new Date();
+    let shouldIncrementIndex = cooldown > 0;
+    let dt = (newTime - time) / 1000;
+    time = newTime;
+
+    cooldown -= dt;
+
+    if (cooldown <= 0) {
+        if (shouldIncrementIndex) {
+            textIndex++;
+        }
+
+        doMorph();
+    } else {
+        doCooldown();
     }
 }
-else if (b > c){
-    console.log(`${b} son katta`);
-}else if(c > b){
-    console.log(`${c} son katta`);
-}else if(a = b = c){
- console.log("sonlar teng");
-}else{
-    console.log('teng');
-}
-console.log(`a son ${a}`);
-console.log(`b son ${b}`);
-console.log(`c son ${c}`);
+
+animate();
